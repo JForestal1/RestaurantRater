a90a2328158c749d7db00d471deab2ee728a0f90
JForestal1 <jforestal1@gmail.com>
2021-03-25 21:58:27 -0400

    create added

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -16,5 +16,27 @@ namespace RestaurantRater.Controllers
         {
             return View(_db.Restaurants.ToList());
         }
+
+        // GET: Restaurant
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Resturant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
     }
 }
